709103323332a29c1dd893434edccf0f4ab6bde7
pebblepark <subling02@gmail.com>
2021-04-06 16:58:31 +0900

    AuthenticationProvider 구현

---
+++ src/main/java/com/spring/security/example/common/security/CustomAuthProvider.java
@@ -0,0 +1,42 @@
+package com.spring.security.example.common.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.spring.security.example.user.dao.User;
+import com.spring.security.example.user.service.UserService;
+
+@Component
+public class CustomAuthProvider implements AuthenticationProvider{
+    @Autowired
+    private UserService userService;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+	String uid = authentication.getName();
+	String password = (String) authentication.getCredentials();
+	
+	User user = (User) userService.loadUserByUsername(uid);
+	
+	if(!passwordEncoder.matches(password, user.getPassword())) {
+	    throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+	}
+	
+	return new UsernamePasswordAuthenticationToken(uid, password, user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+	return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
