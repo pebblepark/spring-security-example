349108cdd517e38e1726fae7d2f82ee23fb7edb7
pebblepark <subling02@gmail.com>
2021-04-06 16:59:01 +0900

    Authentication Handler 구현

---
+++ src/main/java/com/spring/security/example/common/security/CustomSuccessHandler.java
@@ -0,0 +1,23 @@
+package com.spring.security.example.common.security;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomSuccessHandler implements AuthenticationSuccessHandler{
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+	    Authentication authentication) throws IOException, ServletException {
+	response.sendRedirect("/main");
+    }
+
+}
