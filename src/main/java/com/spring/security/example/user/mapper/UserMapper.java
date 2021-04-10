46c52cd3b076601abea0c2fdca554b19cff73b81
pebblepark <subling02@gmail.com>
2021-04-10 14:34:35 +0900

    mybatis 연동 문제 해결

--- src/main/java/com/spring/security/example/user/mapper/UserMapper.java
+++ src/main/java/com/spring/security/example/user/mapper/UserMapper.java
@@ -1,14 +1,15 @@
 package com.spring.security.example.user.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import com.spring.security.example.user.dao.User;
 
 @Mapper
-@Repository
 public interface UserMapper {
 
+    @Select("SELECT * FROM user WHERE uid = #{uid}")
     User loadUserByUsername(String username);
 
 }
